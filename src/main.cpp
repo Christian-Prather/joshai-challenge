b57d21f1ba387bb545470f43da597f7123afded7
Christian Prather <clemsbach@gmail.com>
2024-04-30 14:52:20 -0600

    Add and get familiar with http library

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,41 @@
+// System libraries
 #include <iostream>
 
-int main() {
-  std::cout << "Interface starting..." << std::endl;
-  return 0;
+// Local
+#include "cpp-httplib/httplib.h"
+
+/**
+ * Steps
+ * 1) Read state of all lights
+ * 2) Condition data (brightness 0-255 to 0-100 etc)
+ * 3) Print out to consol json data
+ * 4) Monitor (long pull?) for changes to the server
+ * 5) Update consol with changes
+ * 6) Handle errors (http fails, json fails) -> inform and recover
+ * 7) Docs, Docs, Docs
+ *
+ * Thoughts:
+ * Unit testing will be tricky as this would require a mock server in c++ or a guarantee that the
+ * simulator is running.
+ *
+ * Need to make sure all edge cases are handled
+ * (Maybe a basic event handler abstraction)
+ *
+ * ON/OFF events
+ * Brightness event
+ * ADD/Removal of light
+ *
+ */
+
+int main()
+{
+    std::cout << "Interface starting..." << std::endl;
+
+    httplib::Client cli("http://localhost:8080");
+    auto response = cli.Get("/lights");
+
+    std::cout << response->status << std::endl;
+    std::cout << response->body << std::endl;
+
+    return 0;
 }
